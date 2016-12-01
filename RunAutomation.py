69ecd360b7c82842c7e96e6e275cbdc31c51cf90
ebbitten <Adam.Helitzer@gmail.com>
2016-12-01 10:30:39 -0600

    Revert "Add files via upload"

--- RunAutomation.py
+++ RunAutomation.py
@@ -45,9 +45,9 @@ def findCoordinates(hw,overLoads,prayerpots):
     print(ppCords)
     filePath = input("please input a filepath to save the coordinates")
 
-findCoordinates(1,0,0)
+# findCoordinates(1,7,20)
 
-def runNMZ(oversConsumed=0, ppConsumed=0,overConsumeCounter=310, ppConsumeCounter = 150):
+def runNMZ(oversConsumed=0, ppConsumed=0,overConsumeCounter=310, ppConsumeCounter = 153):
     #TODO upload to Github and start running on other computer
     #Todo: put all coords in one array and then split them based on input
     #TODO load positions from a file
@@ -55,9 +55,8 @@ def runNMZ(oversConsumed=0, ppConsumed=0,overConsumeCounter=310, ppConsumeCounte
 
     startTime = time.time()
     curTime = time.time()
-    overCoords = [[1195, 256], [1237, 259], [1280, 256], [1155, 288], [1194, 294], [1235, 295], [1279, 292]]
-    ppCoords = [[1155, 330], [1194, 329], [1238, 331], [1279, 333], [1155, 368], [1198, 368], [1238, 369], [1280, 365], [1155, 405], [1197, 404], [1239, 402], [1278, 403], [1152, 438], [1196, 438], [1235, 442], [1281, 441], [1154, 477], [1194, 477], [1239, 474], [1280, 476]]
-    rapidHeal = [1293, 295]
+    overCoords = [[878, 254], [921, 253], [961, 251], [832, 293], [876, 290], [917, 290], [960, 292]]
+    ppCoords = [[835, 325], [876, 328], [917, 326], [958, 326], [835, 362], [875, 361], [917, 365], [960, 363], [834, 400], [878, 400], [918, 400], [958, 399], [833, 436], [878, 436], [917, 435], [958, 433], [833, 465], [877, 470], [916, 471], [958, 471]]
     consumedCounter = [overConsumeCounter,ppConsumeCounter]
     if oversConsumed>0:
         for i in range(oversConsumed//4):
@@ -72,7 +71,7 @@ def runNMZ(oversConsumed=0, ppConsumed=0,overConsumeCounter=310, ppConsumeCounte
     timeElapsed = curTime-startTime
     consumedList = [oversConsumed,ppConsumed]
     print("starting in 10")
-    while timeElapsed<12000:
+    while timeElapsed<9000:
         importlib.reload(helperLoop)
         curTime=time.time()
         timeElapsed = curTime - startTime
@@ -80,8 +79,7 @@ def runNMZ(oversConsumed=0, ppConsumed=0,overConsumeCounter=310, ppConsumeCounte
         #print(str(consumedList) + "consumed list")
 
 print("initializing")
-runNMZ(0,0)
-#findCoordinates(1,7,20)
+runNMZ(11,30)
 
 
 
