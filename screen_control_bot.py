bccc25d508d9f3092c5baaa67881ea1f4debd31f
ebbitten <Adam.Helitzer@gmail.com>
2019-09-22 20:13:41 -0500

    add functionality around retrying move commands

--- screen_control_bot.py
+++ screen_control_bot.py
@@ -11,8 +11,10 @@ MAX_FAILED_MOVE_ATTEMPTS = 15
 FAILED_MOVE_ATTEMPTS = 0
 FAILED_MOVE_ATTEMPTS = 0
 MAX_FAILED_MOVE_ATTEMPTS = 0
+
 class ScreenBot():
-    def __init__(self, max_cur_failed_attempts=5, max_total_failed_attempts=15):
+    def __init__(self, max_cur_failed_attempts=3, max_total_failed_attempts=15, text_compare_threshold=70,
+                 take_failed_screen=True, take_success_screen=True):
         self.cur_pos = []
         self.prev_pos = []
         self.cur_fails = 0
@@ -21,6 +23,10 @@ class ScreenBot():
         self.time_sleeping = 0
         self.max_cur_failed_attempts = max_cur_failed_attempts
         self.max_total_failed_attempts = max_total_failed_attempts
+        self.text_compare_threshold = text_compare_threshold
+        self.take_failed_screen = take_failed_screen
+        self.take_success_screen = take_success_screen
+        self.is_moving = False
 
     def click_wait(self, num):
         for i in range(num):
@@ -28,60 +34,76 @@ class ScreenBot():
             self.print_sleep(abs(random.normalvariate(1.4, .1)))
             self.random_sleep()
 
-    def random_sleep(self):
-        if random.random() < .01:
+    def random_sleep(self, multiplier=1):
+        if random.random() < multiplier * .01:
             self.print_sleep(abs(random.normalvariate(40, 5)))
-        if random.random() < .05:
+        if random.random() < multiplier *.05:
             self.print_sleep(abs(random.normalvariate(7, 3)))
-        if random.random() < .002:
+        if random.random() < multiplier * .002:
             print('browsing')
-            # helperLoop.randomBrowsing()
-            print('browsing not currently implemented')
-            # TODO re-implement but make sure it ends up back in the same spot
+            self._random_browsing()
 
-    def easy_move(self, location, text="", take_failed_screenshot=False, threshold=80, max_attempts=3):
-        self.human_move(location[0], location[1], 1.2, 8)
-        time.sleep(.2)
-        global FAILED_MOVE_ATTEMPTS
-        global MAX_FAILED_MOVE_ATTEMPTS
+    def _random_browsing(self):
+        final_x, final_y = self.get_coord()
+        print("Random Browsing")
+        screenWidth, screenHeight = pyautogui.size()
+        for i in range(int(max(random.normalvariate(5, 1),1))):
+            x = random.randint(1, screenWidth)
+            y = random.randint(1, screenHeight)
+            move_time = random.randrange(15, 30, 1) / 10
+            self._human_move(x, y, move_time, 4)
+        self._human_move(final_x, final_y, move_time * 2, 4)
+
+    def print_sleep(self, time_to_sleep):
+        if time_to_sleep > 2:
+            print("time to sleep: ", time_to_sleep)
+        time.sleep(time_to_sleep)
+        if random.random() < .02:
+            print("Sleeping for a bit longer!")
+            self.random_sleep(50)
+
+    def retry_move(self, location_0, location_1):
+        self.print_sleep(.2)
+        self.cur_fails += 1
+        self.total_fails += 1
+        self.print_sleep(.5)
+        if self.total_fails == self.max_total_failed_attempts:
+            raise FailedMoveAttempt('too many failed movements')
+        elif self.cur_fails == self.max_cur_failed_attempts:
+            print("Self total fails: " + str(self.total_fails))
+            self.print_sleep(2)
+            self._random_browsing()
+        else:
+            self._human_move(location_0, location_1, .7, 1, jiggle=True)
+
+    def easy_move(self, location, text=""):
+        self._human_move(location[0], location[1], 1.2, 8)
+        self.print_sleep(.2)
         if text:
-            attempts = 0
-            while attempts < max_attempts:
+            self.is_moving = True
+            self.cur_fails = 0
+            while self.is_moving:
                 time.sleep(.3)
-                if ocr.screen_and_compare(text, threshold, take_failed_screenshot):
+                if ocr.screen_and_compare(text, self.text_compare_threshold, self.take_failed_screen):
+                    self.is_moving = False
                     return True
                 else:
-                    self.human_move(location[0], location[1], .3, 1, jiggle=True)
-                    self.print_sleep(.5)
-                    attempts += 1
-            if attempts == max_attempts:
-                FAILED_MOVE_ATTEMPTS += 1
-                print(FAILED_MOVE_ATTEMPTS)
-            if FAILED_MOVE_ATTEMPTS >= MAX_FAILED_MOVE_ATTEMPTS:
-                raise FailedMoveAttempt('too many failed movements')
+                    self.retry_move(location[0], location[1])
 
     def move_and_decide_text(self, location, text_list, threshold=80, max_attempts=3):
-        self.human_move(location[0], location[1], 1.2, 8)
-        time.sleep(.2)
+        self._human_move(location[0], location[1], 1.2, 8)
+        self.print_sleep(.2)
         self.cur_fails = 0
-        while self.cur_fails < self.max_cur_failed_attempts:
+        self.is_moving = True
+        while self.is_moving:
             top_text_result = ocr.screen_compare_multiple_texts(text_list, threshold)
             if top_text_result:
+                self.is_moving = False
                 return top_text_result
             else:
-                self.human_move(location[0], location[1], .3, 1, jiggle=True)
-                self.print_sleep(.5)
-                self.cur_fails += 1
-                self.total_fails += 1
-        if self.total_fails > self.max_total_failed_attempts:
-            raise FailedMoveAttempt("Too many failed movements")
+                self.retry_move()
 
-
-
-
-
-
-    def human_move(self, finalx, finaly, totalTime, steps, jiggle=False):
+    def _human_move(self, finalx, finaly, totalTime, steps, jiggle=False):
         self.prev_pos = self.cur_pos
         tweens = [pyautogui.easeOutQuad, pyautogui.easeInQuad, pyautogui.easeInOutQuad]
         starting_pos = pyautogui.position()
@@ -94,7 +116,7 @@ class ScreenBot():
         step_variance = 5
         last_variance = 3
         if jiggle:
-            last_variance = 5
+            last_variance = 10
         for i in range(1, steps + 1):
             tween_choice = random.choice(tweens)
             x = starting_pos[0] * (steps - i) / steps + finalx * (i) / steps
@@ -118,7 +140,7 @@ class ScreenBot():
     def easy_right_click(self):
         self.do_click(clicks=1, button='right', duration=(random.normalvariate(35, 3) / 100))
 
-    def do_click(self, clicks=1, interval=0.0, button='left', duration=0.0):
+    def _do_click(self, clicks=1, interval=0.0, button='left', duration=0.0):
         try:
             pyautogui.click(clicks=clicks, interval=interval, button=button, duration=duration)
         except:
@@ -126,11 +148,6 @@ class ScreenBot():
                 pyautogui.click(clicks=clicks, interval=interval, button=button, duration=duration)
             except:
                 pass
-
-    def print_sleep(self, time_to_sleep):
-        if time_to_sleep > 2:
-            print("time to sleep: ", time_to_sleep)
-        time.sleep(time_to_sleep)
 
     def get_coord(self):
         counter = 0
@@ -173,19 +190,12 @@ class ScreenBot():
         print(pp_cords)
         filePath = input("please input a filepath to save the coordinates")
 
-    def randomBrowsing(self):
-        screenWidth, screenHeight = pyautogui.size()
-        x = random.randint(1, screenWidth)
-        y = random.randint(1, screenHeight)
-        moveTime = random.randrange(15, 30, 1) / 10
-        self.human_move(x, y, moveTime, 4)
-
     def flick_pray(self, rapidHeal):
         randInterval = random.randint(20, 30) / 100
         pyautogui.press('f5', interval=randInterval)
         time.sleep(random.randint(25, 35) / 100)
         randInterval = random.normalvariate(25, 3) / 10
-        self.human_move(rapidHeal[0], rapidHeal[1], randInterval, 2)
+        self._human_move(rapidHeal[0], rapidHeal[1], randInterval, 2)
         self.do_click(clicks=1, duration=(random.normalvariate(25, 3) / 100))
         time.sleep(random.randint(150, 200) / 100)
         self.do_click(clicks=1, duration=(random.normalvariate(25, 3) / 100))
@@ -202,13 +212,13 @@ class ScreenBot():
             x = coords_list[consumed_type][0][0] + random.normalvariate(0, 2)
             y = coords_list[consumed_type][0][1] + random.normalvariate(0, 2)
             moveTime = random.randrange(20, 40, 1) / 10
-            self.human_move(x, y, moveTime, 3)
+            self._human_move(x, y, moveTime, 3)
             # click
             self.do_click(duration=random.normalvariate(20, 3) / 100)
             time.sleep(.5)
             # Move to a random location occasionly
             if random.randint(0, 20) > 19:
-                self.random_browsing()
+                self._random_browsing()
             # update our consumption
             consumed_list[consumed_type] += 1
             if consumed_list[consumed_type] % 4 == 0:
@@ -218,7 +228,27 @@ class ScreenBot():
                 coords_list[consumed_type].pop(0)
                 consumed_list[consumed_type] += 1
 
-
+    def record_screen_coords(self, hw, over_loads, prayer_pots):
+        # assume that the first position is holy wrench
+        # the second through 8th position are overloads
+        # the 9th through 28th position are prayer pots
+        hw_coords = []
+        over_coords = []
+        pp_cords = []
+        self.print_sleep(5)
+        for i in range(hw):
+            new_coord = self.get_coord()
+            hw_coords.append(new_coord)
+        for i in range(over_loads):
+            new_coord = self.get_coord()
+            over_coords.append(new_coord)
+        for i in range(prayer_pots):
+            new_coord = self.get_coord()
+            pp_cords.append(new_coord)
+        winsound.Beep(2000, 2000)
+        print(hw_coords)
+        print(over_coords)
+        print(pp_cords)
 
 
 
