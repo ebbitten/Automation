1758bb58787c4da21081cf4ae6ca4f50b1e35e46
ebbitten <adam.helitzer@gmail.com>
2022-09-25 16:09:10 -0500

    no clue

--- game_control/__init__.py
+++ game_control/__init__.py
@@ -1,2 +1,2 @@
-from .beztools import *
+from game_control.mouse_control.beztools import *
 from .screen_control_bot import ScreenBot
--- recorder/mouse_logger.py
+++ recorder/mouse_logger.py
@@ -1,16 +1,24 @@
-
+from datetime import datetime
 import logging
 import time
 
 from pynput.mouse import Listener, Button, Controller
 import pandas as pd
 
+logging.basicConfig(filename=f'../data/mouse_training/{datetime.today().strftime("%d_%m_%Y")}.csv',
+                    level=logging.DEBUG, format='%(message)s')
+
+
 def on_move(x, y):
-    print(f'Mouse moved to ({x}, {y}) at t={time.time()}')
+    print(f'{x}, {y}, {time.time()%(60*60*24)}, 0')
+    logging.info(f'{x}, {y}, {time.time()%(60*60*24)}, 0')
+
 
 def on_click(x, y, button, pressed):
     if pressed:
-        print(f'Mouse clicked at ({x}, {y}) with {button}at t={time.time()}')
+        print(f'{x}, {y}, {time.time()%(60*60*24)}, 1')
+        logging.info(f'{x}, {y}, {time.time()%(60*60*24)}, 1')
+
 
 def on_scroll (x, y, dx, dy):
     print(f'Mouse scrolled at ({x}, {y},) in direction ({dx}, {dy})')
